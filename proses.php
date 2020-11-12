09d190ed0ad3731ff3127d72956ffda8d36d231a
FaustinaPuspamurti <faustinapuspamurti2105@gmail.com>
2020-11-12 14:22:04 +0700

    PHP

---
+++ proses.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Proses</title>
+</head>
+<body>
+	<h1>Halo, <?= $_POST["nama"];?></h1>
+	<h1>Kamu Tinggal di <?= $_POST["alamat"];  ?></h1>
+</body>
+</html>
